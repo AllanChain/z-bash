4225d35c3a1a93f62b2d7d4c91194abf4a5bb5a6
Allan Chain <allanchain@pku.edu.cn>
2019-12-27 14:28:11 +0800

    add cd stack

--- z-bash.sh
+++ z-bash.sh
@@ -2,6 +2,7 @@ export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWUNTRACKEDFILES=1
 SCRIPT_PATH=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
 
+. $SCRIPT_PATH/acd_func.sh
 . $SCRIPT_PATH/zsh-alias.sh
 . $SCRIPT_PATH/git-completion.sh
 
