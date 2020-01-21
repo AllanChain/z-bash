e2e62cfbb39c6b87586d3e8ce12f79a33f0a897e
Allan Chain <allanchain@pku.edu.cn>
2020-01-21 16:43:09 +0800

    add time and temp

--- plain-prompt.sh
+++ plain-prompt.sh
@@ -3,18 +3,26 @@ prompt_section(){
     printf '%s' "$2"
     printf "\001\033[0m\002"
 }
-end_prompt(){
-    if (( $1 != 0 )); then
-        printf "\n\001\033[1;31m\002"
-    else
-        printf "\n\001\033[1;32m\002"
+temp_prompt(){
+    if [ -e /sys/class/thermal/thermal_zone0/temp ]; then
+        temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        if [ $temp -lt 30000 ]; then color=2
+        elif [ $temp -lt 50000 ]; then color=3
+        else color=1
+        fi
+        prompt_section $color "${temp::${#temp}-3}.${temp:${#temp}-3}"
     fi
-    printf ">\001\033[0m\002 "
+}
+clock_prompt(){
+    prompt_section 6 "$(date +'%H:%M:%S')"
+}
+user_prompt(){
+    prompt_section 4 "$USER@$HOSTNAME"
 }
 dir_prompt(){
     d_all=$(pwd)
     d_all=${d_all/$HOME/\~}
-    prompt_section 4 "$d_all"
+    prompt_section 7 "$d_all"
 }
 git_prompt(){
     if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
@@ -39,17 +47,22 @@ git_prompt(){
         fi
     fi
 }
-
-user_prompt(){
-    prompt_section 5 "$USER@$HOSTNAME"
+end_prompt(){
+    if [ $1 != 0 ]; then
+        printf "\n\001\033[1;31m\002"
+    else
+        printf "\n\001\033[1;32m\002"
+    fi
+    printf ">\001\033[0m\002 "
 }
 build_prompt(){
     status=$?
+    temp_prompt
+    clock_prompt
     user_prompt
     dir_prompt
     git_prompt
     end_prompt $status
 }
 
-#PS1='\[\033[30;44m\] \w \[\033[0m\]$(build_prompt)$(dir_prompt)'
 PS1='$(build_prompt)'
