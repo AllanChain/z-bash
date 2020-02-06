34ec34482dfb1ee48821025739a1dbfb9479f820
Allan Chain <allanchain@pku.edu.cn>
2020-02-06 12:52:42 +0800

    do not write special character straight in file

--- agnoster-prompt.sh
+++ agnoster-prompt.sh
@@ -1,7 +1,6 @@
 CURRENT_BG=''
 # Characters
 SEGMENT_SEPARATOR="\ue0b0"
-PLUSMINUS="\u00b1"
 BRANCH="\ue0a0"
 DETACHED="\u27a6"
 CROSS="\u2718"
@@ -11,7 +10,7 @@ GEAR="\u2699"
 prompt_section(){
     if [[ -n $CURRENT_BG ]];then
         # end the last section and draw seprator
-        printf " \001\033[0m\033[3${CURRENT_BG};4$2m\002\001\033[0m\002"
+        printf " \001\033[0m\033[3${CURRENT_BG};4$2m\002$SEGMENT_SEPARATOR\001\033[0m\002"
     fi
     # open current section
     printf "\001\033[3$1;4$2m\002 "
@@ -23,7 +22,7 @@ colored_str(){
     printf "\001\033[3$1m\002$2"
 }
 end_prompt(){
-    printf " \001\033[0m\002\001\033[3${CURRENT_BG}m\002\001\033[0m\002 "
+    printf " \001\033[0m\002\001\033[3${CURRENT_BG}m\002$SEGMENT_SEPARATOR\001\033[0m\002 "
 }
 # Status:
 # - was there an error
@@ -57,9 +56,9 @@ dir_prompt(){
 git_prompt(){
     if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
         if git symbolic-ref HEAD &> /dev/null;then
-            symbol=''
+            symbol=$BRANCH
         else
-            symbol='➦'
+            symbol=$DETACHED
         fi
 
         gps=$(__git_ps1)
