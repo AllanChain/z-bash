5675b651136c761b407a615800398b590db82685
Allan Chain <allanchain@pku.edu.cn>
2020-02-06 10:49:52 +0800

    use sed to trim pwd

--- agnoster-prompt.sh
+++ agnoster-prompt.sh
@@ -11,21 +11,15 @@ prompt_section(){
     export CURRENT_BG=$2
 }
 end_prompt(){
-    printf " \001\033[0m\002\001\033[3${CURRENT_BG}m\002\001\033[0m\002"
+    printf " \001\033[0m\002\001\033[3${CURRENT_BG}m\002\001\033[0m\002 "
 }
 dir_prompt(){
     prompt_section 0 4
     d_all=$(pwd)
     d_all=${d_all/$HOME/\~}
-    printf "$d_all"
-#    if [[ d_all = "~" ]];then
-#        printf '#'
-#    else
-#        d_1="${d_all##*/}"
-#        d_2="${d_all%/*}"
-#        d_2="${d_2##*/}"
-#        printf "${d_2}/${d_1}"
-#    fi
+    # printf "$d_all"
+    d_trim=$(echo $d_all | sed 's|.*\(\(/[^/]*\)\{2\}\)|\1|g' | sed 's|^/||')
+    echo -n $d_trim
 }
 git_prompt(){
     if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
@@ -70,5 +64,4 @@ build_prompt(){
     end_prompt
 }
 
-#PS1='\[\033[30;44m\] \w \[\033[0m\]$(build_prompt)$(dir_prompt)'
 PS1='$(build_prompt)'
