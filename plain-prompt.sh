2805cb8a76f2cfb29852d4fa81dca779216d60e3
Allan Chain <allanchain@pku.edu.cn>
2020-02-07 10:36:39 +0800

    so does plain

--- plain-prompt.sh
+++ plain-prompt.sh
@@ -5,11 +5,11 @@ prompt_section(){
 }
 temp_prompt(){
     if [ -e /sys/class/thermal/thermal_zone0/temp ]; then
-        temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        local temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        local color
         if [ $temp -lt 30000 ]; then color=2
         elif [ $temp -lt 50000 ]; then color=3
-        else color=1
-        fi
+        else color=1; fi
         prompt_section $color "${temp::${#temp}-3}.${temp:${#temp}-3}"
     fi
 }
@@ -20,31 +20,18 @@ user_prompt(){
     prompt_section 4 "$USER@$HOSTNAME"
 }
 dir_prompt(){
-    d_all=$(pwd)
+    local d_all=$(pwd)
     d_all=${d_all/$HOME/\~}
     prompt_section 7 "$d_all"
 }
 git_prompt(){
-    if $(git rev-parse --is-inside-work-tree >/dev/null 2>&1); then
-        repo_path=$(git rev-parse --git-dir 2>/dev/null)
+    local gps=$(__git_ps1)
+    if [[ -n $gps ]]; then
+        gps=${gps:2:-1}
+        local clean=$(echo "$gps" | sed 's|.* .*||')
 
-        if [[ -e "${repo_path}/BISECT_LOG" ]]; then
-          mode=" <B>"
-        elif [[ -e "${repo_path}/MERGE_HEAD" ]]; then
-          mode=" >M<"
-        elif [[ -e "${repo_path}/rebase" || -e "${repo_path}/rebase-apply" || -e "${repo_path}/rebase-merge" || -e "${repo_path}/../.dotest" ]]; then
-          mode=" >R>"
-        fi
-
-        gps=$(__git_ps1)
-        gps="${gps:2:-1}"
-        if ! git diff-index --quiet HEAD 2>/dev/null; then
-            # Uncommitted changes
-            prompt_section 3 "${gps} ${mode}"
-        else
-            # Working directory clean
-            prompt_section 2 "${gps} ${mode}"
-        fi
+        if [[ -n $clean ]]; then prompt_section 2 "$gps"
+        else prompt_section 3 "$gps"; fi
     fi
 }
 end_prompt(){
